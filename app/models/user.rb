3f3ece3be4f664b4d7524ff126c98378bdf85702
Vincent Rehms <v.rehms@gmail.com>
2023-03-08 11:14:36 +0800

    created a follow structure with lewagons guidelines. the user index might not fit into my index feature, since I want to have the lists in the user index. lets's see later. created also routes, views, partials and js file to make the follow structure work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :lists
-  # has_many :followers, class_name: "UserFollow", foreign_key: :following_id
-  # has_many :followings, class_name: "UserFollow", foreign_key: :follower_id
+
   has_many :products, through: :lists
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # def my_followings
-  #   UserFollow.where(follower: self).map(&:following)
-  # end
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
 
-  # def my_followers
-  #   UserFollow.where(following: self).map(&:follower)
-  # end
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
+
+  def is_following?(user_id)
+    relationship = Follow.find_by(follower_id: id, following_id: user_id)
+    return true if relationship
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users, only: [:index] do
+    member do
+      post :follow
+      post :unfollow
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
