c70393390e2e6ece41af9a3c5bbc1032ff117c4f
Vincent Rehms <v.rehms@gmail.com>
2023-03-12 16:38:38 +0800

    created the product create function, along with the new view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,26 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+    @user = current_user
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.logo.attach(params[:product][:logo])
+    @product.photos.attach(params[:product][:photos])
+    if @product.save
+      redirect_to list_path(@product.list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :price, :review, :description, :referrals, :logo, :list_id, photos: [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :lists, only: [:show]
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
