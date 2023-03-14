db7a315580df9142cdc851913dc76e959e4dda7c
Vincent Rehms <v.rehms@gmail.com>
2023-03-14 22:49:38 +0800

    did some general styling improvements, moved stuff around

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     get :follows, on: :member
   end
 
-  resources :lists, only: [:show]
+  resources :lists, only: [:show, :new, :create]
   resources :products, only: [:index, :show, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
