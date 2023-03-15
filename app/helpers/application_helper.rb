c718a78efae7f2f04d024a7c8852b452e010a86e
Vincent Rehms <v.rehms@gmail.com>
2023-03-15 12:42:42 +0800

    started coding the list grid with backhrounds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
+
+
+
 module ApplicationHelper
+  include CloudinaryHelper
+
+  def generate_tiled_background(list, default_color = "gray")
+    products = list.products
+    tiles = []
+
+    products.each do |product|
+      if product.photos.attached?
+        tiles << "<div class='tile' style='background-image: url(#{cl_image_path(product.photos.first.key)})'></div>"
+      end
+    end
+
+    while tiles.count < 8
+      tiles << "<div class='tile' style='background-image: linear-gradient(#{default_color}, #{default_color})'></div>"
+    end
+
+    tiles.join.html_safe
+  end
+
+
+
+
 end
